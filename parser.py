ab011d3975a62b983ecb780932431fc18ae2491e
Artur <faizzer1234@gmail.com>
2023-03-14 15:26:01 +0300

    pandas with lowercase

--- parser.py
+++ parser.py
@@ -15,11 +15,11 @@ text_ar = []
 comment_ar = []
 score_ar = []
 
-#Writing text, comments, scores into arrays
+#Writing text, comments, scores into arrays and making them in lowercase
 for i in range(0, len(jsonObj)):
     for j in range(0,5):
-        text_ar.append(jsonObj['text'][i])
-        comment_ar.append(jsonObj["comments"][i][j]['text'])
+        text_ar.append(jsonObj['text'][i].lower())
+        comment_ar.append(jsonObj["comments"][i][j]['text'].lower())
         score_ar.append(jsonObj['comments'][i][j]['score'])
 
 #Creating dictionary of values for pandas df
