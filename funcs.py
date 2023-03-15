082c9d4555e448d3a195c1c6f82cab04c840b9c7
death-gr1ps <91392090+death-gr1ps@users.noreply.github.com>
2023-03-15 11:42:29 +0300

    change check to less demand

--- funcs.py
+++ funcs.py
@@ -1,14 +1,20 @@
 import nltk
 from nltk.stem import WordNetLemmatizer 
-from textblob import TextBlob, Word
 from nltk.corpus import wordnet
-from nltk.corpus import words
 
 #basic setup
 nltk.download('wordnet')
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 
+def check_numb(word):
+    out = True
+    numb = {'1','2','3','4','5','6','7','8','9', '.', ',', '@', '\\', '/'}
+    for i in word:
+        if i in numb:
+            out = False
+            break
+    return out
 
 def pos_tagger(nltk_tag):
     if nltk_tag.startswith('J'):
@@ -28,7 +34,7 @@ def lemmatizer_func(sentence):
     wordnet_tagged = list(map(lambda x: (x[0], pos_tagger(x[1])), pos_tagged))
     lemmatized_sentence = []
     for word, tag in wordnet_tagged:
-        if word in words.words():
+        if check_numb(word):
             if tag is None:
                 # if there is no available tag, append the token as is
                 lemmatized_sentence.append(word)
